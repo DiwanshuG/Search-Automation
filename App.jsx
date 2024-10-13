9ee9535221b7da5096f259c77d1e97852e167481
Diwanshu Gangwar <118631453+DiwanshuG@users.noreply.github.com>
2024-10-14 03:26:44 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import SearchAutocomplete from "./components/autocomplete";
+function App() {
+  return (
+    <div className="App">
+         {/* Search autocomplete components */}
+
+      <SearchAutocomplete/>
+    </div>
+  );
+}
+
+
+export default App;
